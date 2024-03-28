49b2bd3f838d508ea9b4d305ca69c7577b9fd783
vitor-sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-03-28 08:16:55 -0300

    init commit

---
+++ roteiro de estudo.js
@@ -0,0 +1,7 @@
+//declare variavel numero 
+let numero = 0
+//faço um loop de repetição declarando um limite que sera 10
+while(numero < 10){
+    numero++
+    console.log("o número agora é: " + numero)
+}
